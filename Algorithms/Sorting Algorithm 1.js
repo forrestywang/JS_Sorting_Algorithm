130dd590d758d8738cc24126d6835665b66ab9c1
Forrest Wang <95055762+forrestywang@users.noreply.github.com>
2023-05-28 17:28:38 -0400

    Update Sorting Algorithm 1.js
    Made variable names consistent. Since Str is one of the parameters, I changed sortedString to sortedStr.

--- Algorithms/Sorting Algorithm 1.js
+++ Algorithms/Sorting Algorithm 1.js
@@ -1,29 +1,29 @@
-// Pre-condition: Requires the values of Str and orderString
+// Pre-condition: Requires the values of Str and orderStr
 // Post-condition: Should sort all the characters in Str based on the order of appearance in
-// orderString and leave the unsorted ones at the last.
-function order(Str, orderString) {
+// orderStr and leave the unsorted ones at the last.
+function order(Str, orderStr) {
 		// Variable(s):
     let letter;
-    let sortedString = '';
+    let sortedStr = '';
     
     // Algorithm:
-    // Traverse orderString and store each character:
-    for (let a = 0; a < orderString.length; a++) {
-    		letter = orderString[a];
+    // Traverse orderStr and store each character:
+    for (let a = 0; a < orderStr.length; a++) {
+    		letter = orderStr[a];
         
         // Traverse Str:
         for (let b = 0; b < Str.length; b++) {
-        		// Take out a repeating character and concatonate it into a new String while removing 
-            // it from orderString:
+            // Take out a repeating character and concatonate it into a new string while removing 
+            // it from orderStr:
             if (Str[b] == letter) {
-                sortedString += letter;
+                sortedStr += letter;
                 Str = Str.replace(letter, '');
             }
         }
     }
     
     // Output:
-    console.log(sortedString + Str);
+    console.log(sortedStr + Str);
 }
 
 // Call the method:
